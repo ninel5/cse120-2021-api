f37c15d15a9d0e8ecac15b8696426d7e61f465c5
ninel5 <81684565+ninel5@users.noreply.github.com>
2021-04-12 19:31:23 +0400

    Update server.js

--- server.js
+++ server.js
@@ -78,6 +78,20 @@ app.post('/data/delete', function (req, res) {
   .catch(console.error)
 })
 
+app.post('/data/update', function (req, res) {
+  client.connect()
+  .then(client => {
+    let id = req.body.id;
+    const query = { "_id": ObjectId(id)};
+    client.db('cse120-2021-db').collection('books').updateOne(query, {$set: req.body})
+      .then(result => {
+        res.send({"sttatus":"Updated"});
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(console.error)
+})
+
 app.listen(port, function () {
     console.log('Example app listening on port 3001!')
 })
